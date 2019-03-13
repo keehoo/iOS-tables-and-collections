e544de28c5c68ce65703f7624f1fe52ab6918023
kkubicki <kubicki@MacBook-Pro-Administrator.local>
2019-03-13 12:08:58 +0100

    UITableView updates

--- ListViewTak1/CheckListViewController.swift
+++ ListViewTak1/CheckListViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class CheckListViewController	: UITableViewController {
+    
+    
+    var list = ["Array", "Dupa", "Is this right?"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,11 +19,20 @@ class CheckListViewController	: UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 3
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        if let label = cell.viewWithTag(1000) as? UILabel {
+//            if indexPath.row == 0 {
+                label.text = list[indexPath.row]
+//            } else {
+//                label.text = "Sleep"
+//            }
+        }
+        
         return cell
     }
 }
